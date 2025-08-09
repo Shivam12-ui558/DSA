c58809cf812747998f8da2b543917d3646e76ddd
Shivam kumar singh <shivamkumar12082004@gmail.com>
2025-08-09 18:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.81%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        for(int i = 0; i <= 31 ; i++){
+            if(n==(1<<i))return true;
+        }
+        return false;
+    }
+};
