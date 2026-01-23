d0f95f21a9daad82011425e1c5b0c19e12a87250
Shivam kumar singh <shivamkumar12082004@gmail.com>
2026-01-23 22:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.75%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+     
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        bool negative = (dividend < 0) ^ (divisor < 0);
+
+        long long a = llabs(dividend);
+        long long b = llabs(divisor);
+
+        long long ans = 0;
+
+        while (a >= b) {
+            long long temp = b, multiple = 1;
+
+            while ((temp << 1) <= a) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            a -= temp;
+            ans += multiple;
+        }
+
+        return negative ? -ans : ans;
+    }
+};
