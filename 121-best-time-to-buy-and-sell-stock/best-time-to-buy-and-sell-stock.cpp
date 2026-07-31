7c46a89a0ec684819ba377a3be71303f7d4cb014
Shivam kumar singh <shivamkumar12082004@gmail.com>
2026-07-31 12:59:53 +0530

    Time: 0 ms (100.00%) | Memory: 97.2 MB (88.07%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int minPrice = INT_MAX;
-        int maxProfit = 0;     
-        for (int price : prices) {
-            if (price < minPrice) {
-                minPrice = price;
-            } else {
-                maxProfit = max(maxProfit, price - minPrice);
-            }
+        int maxProfit = 0;
+        for(int price : prices){
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price-minPrice);
         }
-
         return maxProfit;
-    }
+
+     }
 };
